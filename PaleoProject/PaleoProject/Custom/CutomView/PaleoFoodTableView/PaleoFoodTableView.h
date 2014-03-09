302ec206363c29d6aea8f90a0e75b110a1b09dad
renanvs <renan.silva@digitalpages.com.br>
2014-03-09 10:00:31 -0300

    adicionando documentação de exemplo #13

--- PaleoProject/PaleoProject/Custom/CutomView/PaleoFoodTableView/PaleoFoodTableView.h
+++ PaleoProject/PaleoProject/Custom/CutomView/PaleoFoodTableView/PaleoFoodTableView.h
@@ -24,6 +24,20 @@
 
 @property (nonatomic, assign) id <PaleoFoodTableViewDelegate> delegateList;
 
+/**
+ Check whether a file at a given URL has a newer timestamp than a given file.
+ Example usage:
+ @code
+ NSURL *url1, *url2;
+ BOOL isNewer = [FileUtils
+ isThisFileNewerThanThatFile:url1 thatURL:url2];
+ @endcode
+ @see http://www.dadabeatnik.com for more information.
+ @param indexPath
+ The URL of the source file.
+ @return YES if the timestamp of @c thatURL is newer than the timestamp of @c thisURL,
+ otherwise NO.
+ */
 -(FoodItemModel*)getFoodModelByIndexPath:(NSIndexPath*)indexPath;
 -(NSInteger)getNumberOfRowsInSection:(NSInteger)section;
 -(NSInteger)getSectionCount;
